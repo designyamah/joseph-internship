2f1f09934dc0b5ad9a0a26920c823b1fb3ebf805
designyamah <nnyamahjoseph@gmail.com  git config --global user.name designyamahgit config --global user.email nnyamahjoseph@gmail.com>
2025-01-27 15:14:11 +0100

    hot-collection-feature

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import { Axios } from "axios";
 
 const HotCollections = () => {
+  const [hotcollection, setHotCollection] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true); // Set loading while the promise is pending
+    setError(null); // Clear any previous errors
+    const fetchData = async () => {
+      try {
+        const response = await Axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+        );
+        setHotCollection(response.data); // Update state with fetched data
+        console.log(response);
+      } catch (err) {
+        setError(err.message); // Handle errors
+      } finally {
+        setIsLoading(false); // Always unset loading after promise resolves or rejects
+      }
+    };
+
+    fetchData();
+  }, []); // Runs once when the component mounts
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
