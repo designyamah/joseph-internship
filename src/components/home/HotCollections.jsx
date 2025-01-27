36ad53a5c1923e3ce1801b72198caf588322041d
designyamah <nnyamahjoseph@gmail.com  git config --global user.name designyamahgit config --global user.email nnyamahjoseph@gmail.com>
2025-01-27 19:51:55 +0100

    change the height of the text area

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
+import Skeleton from "../UI/Skeleton";
 
 const HotCollections = () => {
   const [hotcollection, setHotCollection] = useState([]);
@@ -48,22 +49,6 @@ const HotCollections = () => {
     fetchData();
   }, []);
 
-  if (isLoading) {
-    return (
-      <div>
-        Loading...
-        <Skeleton width={"100%"} height={200} />
-        <Skeleton width={50} height={50} borderRadius={"50%"} />
-        <Skeleton width={"100px"} height={27} />
-        <Skeleton width={"70px"} height={27} />
-      </div>
-    );
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -76,67 +61,100 @@ const HotCollections = () => {
           </div>
         </div>
 
-        {/* Slider Container */}
-        <div className="keen-slider-container">
-          {/* Left Arrow */}
-          <button
-            className="arrow left-arrow"
-            onClick={() => instanceRef.current?.prev()}
-          >
-            &#8249;
-          </button>
-
-          {/* Keen Slider */}
-
-          <div ref={sliderRef} className="keen-slider">
-            {isLoading
-              ? Array()
-              : hotcollection.map((hotColl) => (
+        {isLoading || error ? (
+          <div className="keen-slider-container">
+            <div ref={sliderRef} className="keen-slider">
+              {Array(6)
+                .fill(0)
+                .map((_, index) => (
                   <div
                     className="keen-slider__slide col-lg-3 col-md-6 col-sm-6 col-xs-12"
-                    key={hotColl.authorId}
+                    key={index}
                     style={{ padding: 0 }}
                   >
                     <div className="nft_coll">
                       <div className="nft_wrap">
-                        <Link to="/item-details">
-                          <img
-                            src={hotColl.nftImage}
-                            className="lazy img-fluid"
-                            alt={hotColl.authorImage}
-                            style={{ height: "100%", width: "100%" }}
-                          />
-                        </Link>
+                        <Skeleton width={"100%"} height={200} />
                       </div>
                       <div className="nft_coll_pp">
-                        <Link to="/author">
-                          <img
-                            className="lazy pp-coll"
-                            src={hotColl.authorImage}
-                            alt=""
-                          />
-                        </Link>
+                        <Skeleton width={50} height={50} borderRadius={"50%"} />
                         <i className="fa fa-check"></i>
                       </div>
                       <div className="nft_coll_info">
-                        <Link to="/explore">
-                          <h4>{hotColl.title}</h4>
-                        </Link>
-                        <span>ERC-{hotColl.code}</span>
+                        <div>
+                          <Skeleton width={"100px"} height={27} />
+                        </div>
+                        <Skeleton width={"70px"} height={27} />
                       </div>
                     </div>
                   </div>
                 ))}
+            </div>
           </div>
+        ) : (
+          <div className="keen-slider-container">
+            <button
+              className="arrow left-arrow"
+              onClick={() => {
+                if (instanceRef.current) instanceRef.current.prev();
+              }}
+            >
+              &#8249;
+            </button>
 
-          {/* Right Arrow */}
-          <button
-            className="arrow right-arrow"
-            onClick={() => instanceRef.current?.next()}
-          >
-            &#8250;
-          </button>
-        </div>
+            <div ref={sliderRef} className="keen-slider">
+              {hotcollection.map((hotColl) => (
+                <div
+                  className="keen-slider__slide col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                  key={hotColl.authorId}
+                  style={{ padding: 0 }}
+                >
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                      <Link to="/item-details">
+                        <img
+                          src={hotColl.nftImage}
+                          className="lazy img-fluid"
+                          alt={hotColl.authorImage}
+                          style={{
+                            height: "100%",
+                            width: "100%",
+                            objectFit: "cover",
+                          }}
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to="/author">
+                        <img
+                          className="lazy pp-coll"
+                          src={hotColl.authorImage}
+                          alt=""
+                        />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to="/explore">
+                        <h4>{hotColl.title}</h4>
+                      </Link>
+                      <span>ERC-{hotColl.code}</span>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <button
+              className="arrow right-arrow"
+              onClick={() => {
+                if (instanceRef.current) instanceRef.current.next();
+              }}
+            >
+              &#8250;
+            </button>
+          </div>
+        )}
       </div>
     </section>
   );
